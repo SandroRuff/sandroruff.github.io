e83bc0d6a36109b520727840906c073c2ed46eb7
SandroRuff <sandro97ruff@gmail.com>
2018-10-06 00:46:24 +0300

    infoWindow/animation: add new features

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const infoWindow = document.querySelector('.info-window');
+const info = document.querySelector('.info-window .notify');
+const infoText = document.querySelector('.info-window .notify p');
 const elementsInProgress = document.querySelectorAll('.in-progress');
 const elementsInProgressLength = elementsInProgress.length;
 
+// Events
 const hideInfoWindow = function() {
-  infoWindow.style.opacity = 0;
+  infoText.style.opacity = 0;
   setTimeout(() => {
-    infoWindow.style.visibility = 'hidden';
+    info.style.width = 0;
+    infoWindow.style.opacity = 0;
+    setTimeout(() => {
+      infoWindow.style.visibility = 'hidden';
+    }, 400);
   }, 400);
 };
 
+const showInfoWindow = function() {
+  document.body.style.pointerEvents = 'none';
+  infoWindow.style.visibility = 'visible';
+  infoWindow.style.opacity = 1;
+  if (document.documentElement.scrollWidth >= 640) {
+    info.style.width = '55vw';
+  } else {
+    info.style.width = '100%';
+  }
+  setTimeout(() => {
+    infoText.style.opacity = 1;
+    document.body.style.pointerEvents = 'auto';
+  }, 800);
+};
+
+// Events Listeners
 for (let i = 0; i < elementsInProgressLength; i++) {
-  elementsInProgress[i].addEventListener('click', () => {
-    infoWindow.style.visibility = 'visible';
-    infoWindow.style.opacity = 1;
-  });
+  elementsInProgress[i].addEventListener('click', () => showInfoWindow());
 }
 
 infoWindow.addEventListener('click', () => {
